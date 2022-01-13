b30bc67f0669e4554d39cbad55800341401ae620
RatushnyiArtem <92036528+RatushnyiArtem@users.noreply.github.com>
2022-01-13 23:45:12 +0100

    bot

---
+++ main.js
@@ -0,0 +1,43 @@
+// alert('Hello world');
+// let a = 5
+// alert(a);
+// const
+// b = 7
+// alert(b);
+// let username = 'Artem';
+//  surname = 'Ratushnyi';
+//  userage = 16;
+//  alert('Hello, ' + username + ' Your ' + userage)
+// let message = prompt('Put in brakets your card number')
+// alert(message);
+// console.log(message)
+// const arr = [2,3,5,7];
+// arr.forEach(i =>console.log(i))
+$(document).ready(function(){
+    $(".botinho").click(function(event){
+        $(".mess").toggleClass("active");
+    });
+ });
+
+alert('Hello, guest. Lets go meet together');
+alert('My name is cleverless bot in this world');
+let name = prompt('What is your name');
+alert('Hello, ' + name + ' I am glad to meet you');
+console.log(name);
+let deal = prompt('What are you doing now?');
+alert('It is really cool act ' + deal + ' I want to do this thing but I cannot :('  );
+console.log(deal);
+let friend = prompt('Do you have a friend and how much')
+alert('Your suprised me it is really cool to have ' + friend + ' friend you are really friendship' )
+console.log(friend);
+let music = prompt('What music do you prefere?');
+alert('Cool I will be sure to listen this ' + music);
+console.log(music);
+alert('I really love rock music maybe we have same interest');
+let interest = prompt('So what do you like to do in your free time?');
+alert('I dont hear this ' + interst + ' but maybe it is inetersting who know');
+console.log(interest);
+alert('But now i am so busy, sorry we can talk another time');
+alert('Bye,bye!');
+
+
